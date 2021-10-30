d7e89aeff963afca4a31d951b665ce9d045bba8f
Ana Pilar <pilarbarba03@gmail.com>
2021-10-30 14:45:31 +0200

    Ahora si

---
+++ HolaMundo/src/hola/holamundo.java
@@ -0,0 +1,14 @@
+package hola;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+		String nombre="Hola Mundo";
+	
+	System.out.println(nombre);
+	
+	
+}
