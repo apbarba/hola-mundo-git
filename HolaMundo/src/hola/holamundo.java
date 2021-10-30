21a10dec886f5cabc53d1466886305e2f5306951
Ana Pilar <pilarbarba03@gmail.com>
2021-10-30 14:49:04 +0200

    Arreglando hola mundo

--- HolaMundo/src/hola/holamundo.java
+++ HolaMundo/src/hola/holamundo.java
@@ -4,11 +4,10 @@ public class holamundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
 		String nombre="Hola Mundo";
 	
-	System.out.println(nombre);
+		System.out.println(nombre);
 	
+	}
 	
 }
